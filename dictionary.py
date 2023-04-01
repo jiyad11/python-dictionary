7027f1de4daa91d23392930effec5d64acc2d8b0
jiyad11 <jiyadmuhammed11@gmail.com>
2023-04-01 15:26:07 +0530

    built-in datatype Dictionary

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+# mydict={'rahul':68,'manu':70,'abhi':95}
+# mydict['rahul']=90
+# mydict['jiyad']=79
+# print(mydict)
+
+mydict={'rahul':[68,65,30,40],"manu":70,'abhi':95}
+# mydict.pop('rahul')
+# del mydict['abhi']
+# del mydict
+# mydict.clear()
+print(mydict['rahul'][1:3])
